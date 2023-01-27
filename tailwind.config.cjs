e6ca3ed96897388d510ceab78ba5d97aae04da3c
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-01-27 14:43:38 -0300

    Añadir type y modificar theme.extend

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'hero-night': 'url("/images/hero-dark.jpg")'
-      }
-    }
-  },
-  plugins: []
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'hero-footer': 'url("/images/hero-footer.jpg")',
+				'hero-night': 'url("/images/hero-dark.jpg")'
+			}
+		}
+	},
+	plugins: []
 }
