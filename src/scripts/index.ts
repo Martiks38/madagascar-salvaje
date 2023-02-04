aa8ba5e62cb0d43baf9ed0a9a45257476e872c7e
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-02-03 22:27:55 -0300

    Modificar el manejo del dark theme

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,68 +1,35 @@
-let headerPage: HTMLElement
+import { increaseRotation } from '@utils/increaseRotation'
+import { toggleTheme } from '@utils/toggleTheme'
+
 let theme = window.localStorage.getItem('theme')
 let rotationIncreaseMoonSun = 0
 
-const increaseRotation = (increase: number, angle: number, selector: string) =>
-	((document.querySelector(selector) as HTMLElement).style.transform = `rotate(${
-		increase * angle
-	}deg)`)
-
 document.addEventListener('DOMContentLoaded', () => {
-	headerPage = document.querySelector('.headerPage') as HTMLElement
+	if (theme === 'light') return
 
-	if (theme === 'light') {
-		rotationIncreaseMoonSun++
+	if (!theme && window.matchMedia('(prefers-color-scheme: light)').matches) {
+		window.localStorage.setItem('theme', 'light')
+	}
 
+	if (theme === 'dark' || window.matchMedia('(prefers-color-scheme: dark)').matches) {
 		increaseRotation(rotationIncreaseMoonSun, 180, '#button-theme')
+		toggleTheme()
 
-		if (window.matchMedia('(prefers-color-scheme: light)').matches) return
-
-		headerPage.classList.remove('dark')
-		headerPage.classList.add('light')
-		document.body.classList.remove('dark')
-		document.body.classList.add('light')
-	}
-
-	if (theme === 'dark') {
-		if (window.matchMedia('(prefers-color-scheme: dark)').matches) return
-
-		headerPage.classList.add('dark')
-		headerPage.classList.remove('light')
-		document.body.classList.add('dark')
-		document.body.classList.remove('light')
-	}
-
-	if (!theme) {
-		if (window.matchMedia('(prefers-color-scheme: light)').matches) {
-			window.localStorage.setItem('theme', 'light')
-
-			increaseRotation(rotationIncreaseMoonSun, 180, '#button-theme')
-		} else {
-			window.localStorage.setItem('theme', 'dark')
-		}
+		window.localStorage.setItem('theme', 'dark')
 	}
 })
 
 document.querySelector('#button-theme')?.addEventListener('click', () => {
 	rotationIncreaseMoonSun++
-
 	increaseRotation(rotationIncreaseMoonSun, 180, '#button-theme')
 
 	if (theme === 'dark') {
-		headerPage.classList.add('light')
-		headerPage.classList.remove('dark')
-
-		document.body.classList.remove('dark')
-		document.body.classList.add('light')
+		toggleTheme()
 
 		window.localStorage.setItem('theme', 'light')
 		theme = 'light'
 	} else if (theme === 'light') {
-		headerPage.classList.add('dark')
-		headerPage.classList.remove('light')
-
-		document.body.classList.add('dark')
-		document.body.classList.remove('light')
+		toggleTheme()
 
 		window.localStorage.setItem('theme', 'dark')
 		theme = 'dark'
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,9 @@ module.exports = {
 				'hero-footer': 'url("/images/hero-footer.jpg")',
 				'hero-night': 'url("/images/hero-dark.jpg")'
 			},
+			colors: {
+				'white-80': 'rgb(255 255 255 / 0.80)'
+			},
 			gridTemplateColumns: {
 				suggestions: 'repeat(auto-fit, minmax(auto, 18rem))'
 			}
